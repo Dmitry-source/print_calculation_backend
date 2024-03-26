6f270e8d18277b01b43ea4ee5b16e85f82d5d2a0
Dmitry-source <morgoth89@mail.ru>
2024-03-26 14:41:07 +0400

    test

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .addTag('Azura')
     .addBearerAuth()
     .build();
+    ////////////
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document, {
